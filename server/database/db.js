ef342b9d88a4deb6dba94ffea15c9e579a83df20
abhay227 <abhaypandey264209@gmail.com>
2024-05-18 12:00:37 +0530

    added ignore file

--- server/database/db.js
+++ server/database/db.js
@@ -3,9 +3,10 @@ import dotenv from 'dotenv'
 
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
+const db_url = process.env.DATABASE_URL;
 
 const Connection = async ()=>{
-    const URL = 'mongodb://abhaypandey264209:AbhayPandey%40264209@ac-xr5w3c8-shard-00-00.v10ersx.mongodb.net:27017,ac-xr5w3c8-shard-00-01.v10ersx.mongodb.net:27017,ac-xr5w3c8-shard-00-02.v10ersx.mongodb.net:27017/?ssl=true&replicaSet=atlas-c2s41a-shard-0&authSource=admin&retryWrites=true&w=majority'
+    const URL = db_url;
     try{
        await mongoose.connect(URL,{useUnifiedTopology: true});
        console.log('Database Connected Successfully');
